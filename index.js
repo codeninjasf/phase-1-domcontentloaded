dce406d25a9be7d6741ee1dbbf444258d9095386
arturoalba <ajdalba@gmail.com>
2022-05-08 00:16:15 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+// AJ ALBA - Flatiron School- Software Engineering
+// DOMContentLoaded Lab
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('p').textContent = "This is really cool!";
+  });  
